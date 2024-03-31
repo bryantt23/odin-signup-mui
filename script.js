b7371abc8c95cbb166e480fb8f7150c2a2f28155
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-03-31 01:30:02 -0700

    add image & style sheet

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 // Your React code using MUI components will go here
-const { Container, Grid, Paper } = MaterialUI;
+const { Container, Grid, Paper, Box } = MaterialUI;
 
 function App() {
     return (
-        <Grid container>
-            <Grid item sm={5}>
-                <Paper style={{ backgroundColor: "blue" }}>Item 1</Paper>
+        <Grid container
+            sx={{
+                border: '1px solid black',
+                height: '100vh',
+                margin: 0
+            }}>
+            <Grid item sm={5}
+            // sx={{ backgroundImage: './leaves.jpg' }}
+            >
+
+                <Box
+                    sx={{
+                        backgroundImage: "url('./leaves.jpg')",
+                        backgroundRepeat: "no-repeat",
+                        // minHeight: 'fit-content',
+                        // width: '385px'
+                        height: '100vh',
+                        backgroundSize: 'cover'
+                    }}
+                >
+
+                </Box>
             </Grid>
             <Grid item sm={7}>
                 <Paper style={{ backgroundColor: "grey" }}>Item 2</Paper>
