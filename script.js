5e876b81f8c513504a89407e8299565f252e9583
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-03-31 01:10:00 -0700

    first grid

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert('hii')
+// Your React code using MUI components will go here
+const { Container, Grid, Paper } = MaterialUI;
+
+function App() {
+    return (
+        <Grid container>
+            <Grid item sm={5}>
+                <Paper style={{ backgroundColor: "blue" }}>Item 1</Paper>
+            </Grid>
+            <Grid item sm={7}>
+                <Paper style={{ backgroundColor: "grey" }}>Item 2</Paper>
+            </Grid>
+        </Grid>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
